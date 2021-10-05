212e90e92473d759437ff5e6a55884eaa7fccba8
jtluo05 <jtluo05@gmail.com>
2021-10-04 19:34:04 -0700

    something

--- src/_04_Thread_Pool/tests/Runner.java
+++ src/_04_Thread_Pool/tests/Runner.java
@@ -4,6 +4,13 @@ public class Runner {
 public static void main(String[] args) {
 	WorkQueue wq= new WorkQueue();
 	System.out.println("Total threads: "+ wq.getThreadCount());
+	try {
+		Thread.sleep(1000);
+	} catch(InterruptedException e) {
+		
+	}
+	
 	wq.shutDown();
+	
 }
 }
--- src/_04_Thread_Pool/tests/WorkQueue.java
+++ src/_04_Thread_Pool/tests/WorkQueue.java
@@ -1,39 +1,86 @@
 package _04_Thread_Pool.tests;
 
+import java.lang.Thread.State;
+import java.util.ArrayDeque;
 import java.util.Iterator;
 
-public class WorkQueue implements Runnable{
-private Thread[] threads;
-private boolean isRunning=true;
+public class WorkQueue implements Runnable {
+	private ArrayDeque<Job> jobQueue = new ArrayDeque<Job>();
+	private Thread[] threads;
+	private volatile boolean isRunning = true;
 
-public WorkQueue(){
-	int totalThreads= Runtime.getRuntime().availableProcessors()-1;
-	threads= new Thread[totalThreads];
-}
+	public WorkQueue() {
+		int totalThreads = Runtime.getRuntime().availableProcessors() - 1;
+		threads = new Thread[totalThreads];
+	}
+
+	public int getThreadCount() {
+		return threads.length;
+	}
+
+	public void shutDown() {
+		completeAllJobs();
+		isRunning = false;
+		synchronized (jobQueue) {
+			jobQueue.notifyAll();
+		}
+	}
+
+	private void addJod(Job j) {
+		synchronized (jobQueue) {
+			jobQueue.add(j);
+			jobQueue.notify();
+		}
+	}
+
+	public boolean performJob() {
+		Job j = null;
+		synchronized (jobQueue) {
+			if (!jobQueue.isEmpty()) {
+				j = jobQueue.remove();
+
+				if (j != null) {
+					j.preform();
+					return true;
+				}
+
+			}
+
+		}
+		return false;
+	}
+
+	 public void completeAllJobs() {
+		 while(!jobQueue.isEmpty()) {
+			 performJob();
+		 }
+		 for (int i = 0; i < threads.length; i++) {
+			if (threads[i].getState()!=State.WAITING) {
+				i=-1;
+			}
+		}
+	 }
 	
-public int getThreadCount() {
-	return threads.length;
-}
-
-public void shutDown() {
-	isRunning=false;
-}
-
-
-
 	@Override
 	public void run() {
-		while(isRunning) {
-			System.out.println("Output from thread #" + Thread.currentThread().getId());
+		while (isRunning) {
+			if (!performJob()) {
+				synchronized (jobQueue) {
+					try {
+						jobQueue.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
 		}
 		// TODO Auto-generated method stub
 		for (int i = 0; i < threads.length; i++) {
 			threads[i] = new Thread(this);
 			threads[i].start();
 		}
-		
-		
-		
+
 	}
 
 }
